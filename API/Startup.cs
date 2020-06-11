328747175e42415dfe863bba1518e74a09bf11c4
Jay Hernandez <ajuan_jose@hotmail.com>
2020-06-11 15:12:29 -0600

    Semantic UI added

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,12 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt=> {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
 
         }
@@ -47,6 +53,8 @@ namespace API
            // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
