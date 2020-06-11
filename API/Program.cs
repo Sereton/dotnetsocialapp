375e1e79c9f0aa14b7f4b0bd12bc736e0e0de765
Jay Hernandez <ajuan_jose@hotmail.com>
2020-06-11 04:55:11 -0600

    Persisence files connected to API

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -14,7 +17,25 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+
+           using(var scope = host.Services.CreateScope()){
+               var services = scope.ServiceProvider;
+               try
+               {
+                   var context = services.GetRequiredService<DataContext>();
+                   context.Database.Migrate();
+               }
+               catch (Exception ex) 
+               {
+                   
+                  var logger = services.GetRequiredService<ILogger<Program>>();
+                  logger.LogError(ex, "An error ocurred during migration");
+                  
+               }
+           }
+
+           host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
