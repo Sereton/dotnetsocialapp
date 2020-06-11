5589039c9be7245f29c1ce7702e6bbb43c65e016
Jay Hernandez <ajuan_jose@hotmail.com>
2020-06-11 10:42:26 -0600

    db seeded

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,5 +12,14 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id=1, Name="Pamchito"},
+                    new Value {Id=2, Name="Chuchito"},
+                    new Value {Id=3, Name="Cheito"}
+                );
+        }
     }
 }
