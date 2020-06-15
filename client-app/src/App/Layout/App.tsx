cf969fee7ad70c57e56a542e03fd5c12ce7b01cc
Jay Hernandez <ajuan_jose@hotmail.com>
2020-06-15 14:01:35 -0600

    Added IActivity Interface

--- client-app/src/App/Layout/App.tsx
+++ client-app/src/App/Layout/App.tsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import { Header, Icon, List, ListItem} from 'semantic-ui-react';
-import './App.css';
+
 import axios from 'axios';
+import { IActivity } from '../Models/activity';
 
 
+interface IState{
+  activities: IActivity[];
+}
 
-class App extends Component {
+class App extends Component<{}, IState> {
 
 
-  state = {
+  readonly state: IState = {
     activities: [],
+    
   }
 
+ 
   componentDidMount(){
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
     .then((response)=>{
       
-      this.setState({activities:response.data});
+      this.setState( {activities: response.data} );
+
       
-    })
+    });
     
   }
  
@@ -33,7 +40,7 @@ class App extends Component {
         <Header.Content>PIMPOM</Header.Content>
       </Header>
         <List>
-        {this.state.activities.map((acti: any)=>(<ListItem key={acti.id}>La actividad {acti.name} sera en el venue {acti.venue}</ListItem>))}
+        {this.state.activities.map((acti)=>(<ListItem key={acti.id}>La actividad cool {acti.title} sera en el venue {acti.venue}</ListItem>))}
         </List>
       </div>
     );
